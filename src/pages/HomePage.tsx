6bce22585a294e9e38d66fca76158d19518c35cb
tanto <tantows001@gmail.com>
2024-02-22 13:45:14 +0700

    feat: Slicing Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,20 @@ import Header from '../parts/Header'
 import Hero from '../parts/Hero'
 import JustArrived from '../parts/homepage/JustArrived'
 import BrowseRoom from '../parts/homepage/BrowseRoom'
+import Clients from '../parts/Clients'
+import Sitemap from '../parts/Sitemap'
+import Footer from '../parts/Footer'
 
 export default function HomePage() {
     return (
         <>
             <Header />
             <Hero />
-            <BrowseRoom/>
+            <BrowseRoom />
             <JustArrived />
+            <Clients />
+            <Sitemap />
+            <Footer />
         </>
     )
 }
